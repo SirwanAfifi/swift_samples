5735bd9572bb4506b343cf43e7c9a54b58f8e24b
Sirwan <sir1afifi@gmail.com>
2020-07-26 12:05:54 +0430

    Init

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,15 @@
+func printString(_ string: String) {
+    print("Printing a string: \(string)")
+}
+
+let stringPrinterFunction = printString
+
+stringPrinterFunction("Hi My Name is Sirwan")
+
+func Sum(_ a: Int, _ b: Int) -> Int {
+    return a + b;
+}
+
+let addTwoNumbers = Sum
+
+addTwoNumbers(1, 4);
